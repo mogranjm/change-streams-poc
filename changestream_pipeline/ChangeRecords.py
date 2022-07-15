0999298b7a2fa4853fefd8a6a5f25bf855bbc28d
James Morgan <ja.morgan1@outlook.com>
2022-07-15 14:03:51 +1000

    DataChangeRecord data structure

--- changestream_pipeline/ChangeRecords.py
+++ changestream_pipeline/ChangeRecords.py
@@ -6,3 +6,16 @@ import datetime
 @dataclass()
 class ChangeRecord:
     timestamp: datetime
+
+
+class DataChangeRecord(ChangeRecord):
+    record_sequence: str
+    transaction_id: str
+    is_last_record_in_transaction_partition: bool
+    table_name: str
+    column_types: List[Dict[str, str, bool, int]]
+    changed_values: List[Dict[Dict, Dict, Dict]]
+    change_type: str
+    value_capture_type: str
+    records_in_transaction: int
+    partitions_in_transaction: int
