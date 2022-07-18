09b9b10564b626280c5c79f65dbbee9287a10d09
James Morgan <ja.morgan1@outlook.com>
2022-07-18 20:20:04 +1000

    wrong function used

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "google_cloud_scheduler_job" "spanner_insert_random_user_trigger" {
 
   pubsub_target {
     topic_name = google_pubsub_topic.spanner_insert_random_user_topic.name
-    data = base64decode("new-user")
+    data = base64encode("new-user")
   }
 }
 
